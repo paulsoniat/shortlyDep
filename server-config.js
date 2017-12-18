17afde55d78f318686f3e8eed0652ac491c5179a
Paul Soniat <paulmsoniat@gmail.com>
2017-12-18 08:11:47 -0600

    slight cleanup

--- server-config.js
+++ server-config.js
@@ -15,7 +15,7 @@ app.use(partials());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-app.use(cookieParser('shhhh, very secret'));
+app.use(cookieParser('shhhh, secret'));
 app.use(session({
   secret: 'shhh, it\'s a secret',
   resave: false,
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var app = require('./server-config.js');
 var port;
+
 if (process.env.NODE_STATE === 'development') {
   port = 4568;
 } else {
   port = 80;
 }
-
-// port = 4568;
 
 app.listen(port);
 
